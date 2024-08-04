672db389d6752cb020b6c2bff7a817197ef6544e
o-jill <nobaokix@gmail.com>
2024-08-04 20:33:06 +0900

    beautified code.

--- src/main.rs
+++ src/main.rs
@@ -45,6 +45,108 @@ fn net(vs : &nn::Path) -> impl Module {
         .add(nn::linear(vs / "layer2", HIDDENSIZE, 1, Default::default()))
 }
 
+fn loadkifu(files : &[String]) -> Vec<(bitboard::BitBoard, i8, i8, i8, i8)> {
+    let mut boards : Vec<(bitboard::BitBoard, i8, i8, i8, i8)> = Vec::new();
+    for fname in files.iter() {
+        let path = format!("kifu/{}", fname);
+        print!("{path}\r");
+        let content = std::fs::read_to_string(&path).unwrap();
+        let lines:Vec<&str> = content.split('\n').collect();
+        let kifu = kifu::Kifu::from(&lines);
+        for t in kifu.list.iter() {
+            let ban = bitboard::BitBoard::from(&t.rfen).unwrap();
+            let (fsb, fsw) = ban.fixedstones();
+
+            let ban90 = ban.rotate90();
+            let ban180 = ban.rotate180();
+            let ban270 = ban180.rotate90();
+            boards.push((ban, t.teban, fsb, fsw, kifu.score.unwrap()));
+            boards.push((ban90, t.teban, fsb, fsw, kifu.score.unwrap()));
+            boards.push((ban180, t.teban, fsb, fsw, kifu.score.unwrap()));
+            boards.push((ban270, t.teban, fsb, fsw, kifu.score.unwrap()));
+        }
+    }
+    println!();
+    boards
+}
+
+fn dedupboards(boards : &mut Vec<(bitboard::BitBoard, i8, i8, i8, i8)>) {
+    boards.sort_by(|a, b| {
+        a.0.black.cmp(&b.0.black).then(a.0.white.cmp(&b.0.white))
+    });
+    boards.dedup_by(|a, b| {a == b});
+    println!("board: {} boards", boards.len());
+}
+
+fn extractboards(boards : &[(bitboard::BitBoard, i8, i8, i8, i8)])
+        -> Vec<f32> {
+    boards.iter().map(|(b, t, fb, fw, _s)| {
+        let mut v = [0.0f32 ; INPUTSIZE as usize];
+        for x in 0..8 {
+            for y in 0..8 {
+                v[x * bitboard::NUMCELL + y] = b.at(x as u8, y as u8) as f32;
+            }
+        }
+        v[bitboard::CELL_2D] = *t as f32;
+        v[bitboard::CELL_2D + 1] = *fb as f32;
+        v[bitboard::CELL_2D + 2] = *fw as f32;
+        v
+    }).collect::<Vec<[f32 ; INPUTSIZE as usize]>>().concat()
+}
+
+fn extractscore(boards : &[(bitboard::BitBoard, i8, i8, i8, i8)]) -> Vec<f32> {
+    boards.iter().map(|(_b, _t, _fb, _fw, s)| *s as f32).collect::<Vec<f32>>()
+}
+
+fn storeweights(vs : VarStore) {
+    println!("save to weight.safetensors");
+    vs.save("weight.safetensors").unwrap();
+
+    // VarStore to weights
+    let weights = vs.variables();
+    let mut outp = [0.0f32 ; (INPUTSIZE * HIDDENSIZE) as usize];
+    let mut params = String::from("# 64+1+2-16-1\n");
+    let mut paramste = String::new();
+    let mut paramsfb = String::new();
+    let mut paramsfw = String::new();
+
+    let l1w = weights.get("layer1.weight").unwrap();
+    println!("layer1.weight:{:?}", l1w.size());
+    let numel = l1w.numel();
+    l1w.copy_data(outp.as_mut_slice(), numel);
+    for i in 0..HIDDENSIZE {
+        let offset = (i * INPUTSIZE) as usize;
+        params += &outp[offset..(offset + bitboard::CELL_2D)].iter()
+            .map(|a| format!("{a},")).collect::<Vec<String>>().join("");
+        paramste += &format!("{},", outp[bitboard::CELL_2D + offset]);
+        paramsfb += &format!("{},", outp[bitboard::CELL_2D + 1 + offset]);
+        paramsfw += &format!("{},", outp[bitboard::CELL_2D + 2 + offset]);
+    }
+    params += &paramste;
+    params += &paramsfb;
+    params += &paramsfw;
+    // let keys = ["layer1.weight", "layer1.bias", "layer2.weight", "layer2.bias"];
+    let keys = ["layer1.bias", "layer2.weight"];
+    for key in keys {
+        let l1w = weights.get(key).unwrap();
+        println!("{key}:{:?}", l1w.size());
+        let numel = l1w.numel();
+        l1w.copy_data(outp.as_mut_slice(), numel);
+        params += &outp[0..numel].iter()
+            .map(|a| format!("{a}")).collect::<Vec<String>>().join(",");
+        params += ",";
+    }
+    let l1w = weights.get("layer2.bias").unwrap();
+    println!("layer2.bias:{:?}", l1w.size());
+    let numel = l1w.numel();
+    l1w.copy_data(outp.as_mut_slice(), numel);
+    params += &outp[0..numel].iter()
+        .map(|a| format!("{a}")).collect::<Vec<String>>().join(",");
+    println!("save to weight.txt");
+    let mut f = std::fs::File::create("weights.txt").unwrap();
+    f.write_all(params.as_bytes()).unwrap();
+}
+
 fn main() -> Result<(), tch::TchError> {
     let t = Tensor::f_rand([1, 8, 8], (Kind::Float, Device::Cpu))?;
     // let t = Tensor::from_slice(&[3, 1, 4, 1, 5]);
@@ -89,31 +191,15 @@ fn main() -> Result<(), tch::TchError> {
         }
     }
     println!();
-    boards.sort_by(|a, b| {
-            a.0.black.cmp(&b.0.black).then(a.0.white.cmp(&b.0.white))
-        });
-    boards.dedup_by(|a, b| {a == b});
-    println!("board: {} boards", boards.len());
+    let mut boards = loadkifu(&files);
+    dedupboards(&mut boards);
 
-    let inputlist = boards.iter().map(|(b, t, fb, fw, _s)| {
-        let mut v = [0.0f32 ; INPUTSIZE as usize];
-        for x in 0..8 {
-            for y in 0..8 {
-                v[x * bitboard::NUMCELL + y] = b.at(x as u8, y as u8) as f32;
-            }
-        }
-        v[bitboard::CELL_2D] = *t as f32;
-        v[bitboard::CELL_2D + 1] = *fb as f32;
-        v[bitboard::CELL_2D + 2] = *fw as f32;
-        v
-    }).collect::<Vec<[f32 ; INPUTSIZE as usize]>>().concat();
     let input = tch::Tensor::from_slice(
-        &inputlist).view((boards.len() as i64, INPUTSIZE));
+        &extractboards(&boards)).view((boards.len() as i64, INPUTSIZE));
     println!("input : {} {:?}", input.dim(), input.size());
-    let targetlist = boards.iter()
-        .map(|(_b, _t, _fb, _fw, s)| *s as f32).collect::<Vec<f32>>();
+
     let target = tch::Tensor::from_slice(
-        &targetlist).view((boards.len() as i64, 1));
+        &extractscore(&boards)).view((boards.len() as i64, 1));
     println!("target: {} {:?}", target.dim(), target.size());
 
     let mut vs = VarStore::new(Device::Cpu);
@@ -164,52 +250,9 @@ fn main() -> Result<(), tch::TchError> {
             std::io::stdout().flush().unwrap();
         }
     }
-    println!("save to weight.safetensors");
-    vs.save("weight.safetensors").unwrap();
 
     // VarStore to weights
-    let weights = vs.variables();
-    let mut outp = [0.0f32 ; (INPUTSIZE * HIDDENSIZE) as usize];
-    let mut params = String::from("# 64+1+2-16-1\n");
-    let mut paramste = String::new();
-    let mut paramsfb = String::new();
-    let mut paramsfw = String::new();
-
-    let l1w = weights.get("layer1.weight").unwrap();
-    println!("layer1.weight:{:?}", l1w.size());
-    let numel = l1w.numel();
-    l1w.copy_data(outp.as_mut_slice(), numel);
-    for i in 0..HIDDENSIZE {
-        let offset = (i * INPUTSIZE) as usize;
-        params += &outp[offset..(offset + bitboard::CELL_2D)].iter()
-            .map(|a| format!("{a},")).collect::<Vec<String>>().join("");
-        paramste += &format!("{},", outp[bitboard::CELL_2D + offset]);
-        paramsfb += &format!("{},", outp[bitboard::CELL_2D + 1 + offset]);
-        paramsfw += &format!("{},", outp[bitboard::CELL_2D + 2 + offset]);
-    }
-    params += &paramste;
-    params += &paramsfb;
-    params += &paramsfw;
-    // let keys = ["layer1.weight", "layer1.bias", "layer2.weight", "layer2.bias"];
-    let keys = ["layer1.bias", "layer2.weight"];
-    for key in keys {
-        let l1w = weights.get(key).unwrap();
-        println!("{key}:{:?}", l1w.size());
-        let numel = l1w.numel();
-        l1w.copy_data(outp.as_mut_slice(), numel);
-        params += &outp[0..numel].iter()
-            .map(|a| format!("{a}")).collect::<Vec<String>>().join(",");
-        params += ",";
-    }
-    let l1w = weights.get("layer2.bias").unwrap();
-    println!("layer2.bias:{:?}", l1w.size());
-    let numel = l1w.numel();
-    l1w.copy_data(outp.as_mut_slice(), numel);
-    params += &outp[0..numel].iter()
-        .map(|a| format!("{a}")).collect::<Vec<String>>().join(",");
-    println!("save to weight.txt");
-    let mut f = std::fs::File::create("weights.txt").unwrap();
-    f.write_all(params.as_bytes()).unwrap();
+    storeweights(vs);
 
     Ok(())
 }
