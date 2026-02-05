397e6ea4b71d34e94c705bd64c6075e3991bb6cb
o-jill <nobaokix@gmail.com>
2026-02-05 15:44:55 +0900

    added options to extract mate positions.

--- src/argument.rs
+++ src/argument.rs
@@ -55,10 +55,33 @@ pub struct Arg {
     /// show ascii graph
     #[arg(long, default_value_t = false)]
     pub graph : bool,
-    /// get mate2 positions from extrace mate3.
-    #[arg(long, default_value_t = false)]
-    pub extract_mate3 : bool,
+    /// get mate(N-1) positions by extracting mateN.
+    #[arg(long)]
+    pub extract_mate : Option<String>,
+    /// ruversi config file
+    #[arg(long)]
+    pub ru_config : Option<String>,
     /// paths of files which contains mate boards.
     #[arg(long)]
     pub mate_file : Option<String>,
+}
+
+impl Arg {
+    #[allow(dead_code)]
+    //// `--extract-mate`が指定されたかどうか
+    /// 棋譜から子供の局面を展開するモードにするか否か
+    pub fn is_extract_mate(&self) -> bool {
+        self.extract_mate.is_some()
+    }
+
+    /// `--extract-mate N`で指定した数字Nを返す。
+    /// 指定していないときはゼロを返す。
+    pub fn extract_mate_n(&self) -> u32 {
+        match self.extract_mate.as_ref() {
+            None => {0},
+            Some(n) => {
+                n.parse().unwrap()
+            }
+        }
+    }
 }
