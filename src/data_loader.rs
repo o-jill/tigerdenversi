a583fe4746f191245d9908ac1d4e965597eedf6a
o-jill <nobaokix@gmail.com>
2026-02-05 15:34:05 +0900

    removed unnecessary comments.

--- src/data_loader.rs
+++ src/data_loader.rs
@@ -41,9 +41,7 @@ pub fn loadkifu(files : &[String], d : &str, progress : usize,
         let kifu = kifu::Kifu::from(&lines);
         kifu.list.par_iter().filter_map(|t| {
             let ban = bitboard::BitBoard::from(&t.rfen).unwrap();
-            // 最後の局面とか覚えたい進行度の時じゃない
-            // if ban.is_full() || !ban.is_progress(progress) {return None;}
-            // 最後の局面とか後言っての局面とか覚えたい進行度の時じゃない
+            // 最後の局面とか後一手の局面とか覚えたい進行度の時じゃない
             if ban.is_last1_or_full() || !ban.is_progress(progress) {
                 return None;
             }
